1f3c8d0b93948101d4aee7b776db561a8e808f1d
Smith <fengfei153@aliyun.com>
2019-05-30 14:58:42 +0800

    build: add fixed-picker cascade

--- src/modules/cascade/index.js
+++ src/modules/cascade/index.js
@@ -1,9 +1,13 @@
+import FixedPicker from '../../components/cascade/fixed-picker.vue'
 import Cascade from '../../components/cascade/cascade.vue'
 import Locale from '../../common/locale'
 
 Cascade.install = function (Vue) {
+  Vue.component(FixedPicker.name, FixedPicker)
   Vue.component(Cascade.name, Cascade)
   Locale.install(Vue)
 }
+
+Cascade.FixedPicker = FixedPicker
 
 export default Cascade
